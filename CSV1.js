90344db69807091645a0b67cb4fa606c2d3901e7
oscarfong <oscar.fong@gmail.com>
2019-06-05 17:35:32 +0800

    1 file modified

--- CSV1.js
+++ CSV1.js
@@ -10,6 +10,37 @@ class CSV1 extends Component {
     };
   }
 
+editDistance(s1, s2) {
+  // https://stackoverflow.com/questions/10473745/compare-strings-javascript-return-of-likely
+
+  s1 = s1.toLowerCase();
+  s2 = s2.toLowerCase();
+
+  var costs = new Array();
+  for (var i = 0; i <= s1.length; i++) {
+    var lastValue = i;
+    for (var j = 0; j <= s2.length; j++) {
+      if (i == 0)
+        costs[j] = j;
+      else {
+        if (j > 0) {
+          var newValue = costs[j - 1];
+          if (s1.charAt(i - 1) != s2.charAt(j - 1))
+            newValue = Math.min(Math.min(newValue, lastValue),
+              costs[j]) + 1;
+          costs[j - 1] = lastValue;
+          lastValue = newValue;
+        }
+      }
+    }
+    if (i > 0)
+      costs[s2.length] = lastValue;
+  }
+  return costs[s2.length];
+}
+
+
+
 render() {
     return (
       <div>
@@ -25,6 +56,8 @@ render() {
         {this.state.name}
         <p/>
         {this.props.csvLine1}
+        <p/>
+        {this.editDistance("aaa1", "aba") }
 
 
       </div>
