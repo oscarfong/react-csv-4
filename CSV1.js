d3087859658021272094838b9510177b98645d6f
oscarfong <oscar.fong@gmail.com>
2019-06-05 17:18:29 +0800

    Initial commit

---
+++ CSV1.js
@@ -0,0 +1,35 @@
+
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class CSV1 extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'CSV1 in React'
+    };
+  }
+
+render() {
+    return (
+      <div>
+        
+        <p>
+          Start editing to see some magic happen :)
+          <p/>
+          Test to see if stackblitz reverts my code.
+        </p>
+
+        <div csvLine1='1,3,"apple"'>
+        </div>
+        {this.state.name}
+
+
+      </div>
+      
+    );
+  }
+}
+
+//render(<CSV1 />, document.getElementById('root1'));
+export default CSV1;
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-csv-4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-csv-4)
