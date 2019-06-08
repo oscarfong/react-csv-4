e56d000ded2d333f328fcab9ea36da59935da94a
oscarfong <oscar.fong@gmail.com>
2019-06-08 12:03:44 +0800

    SingleCell 's else case

--- SingleCell.js
+++ SingleCell.js
@@ -16,10 +16,17 @@ class SingleCell extends Component {
     </div>
   }
 
+  renderTerminalCase() {
+    return <div>
+    </div>
+  }
+
   render(){
     if (this.props.value !="" ) // how value is passed from TableCSV to SingleCell
     {
       return this.renderSomething()
+    } else {
+      return this.renderTerminalCase()
     }
   }
 }
