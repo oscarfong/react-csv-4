f06a37478970769b840415cfcba21be241ffe292
oscarfong <oscar.fong@gmail.com>
2019-06-08 14:26:32 +0800

    cleaner code of SingleCell.js

--- SingleCell.js
+++ SingleCell.js
@@ -16,6 +16,7 @@ class SingleCell extends Component {
           value={firstCSVArg}
           onChange={event => this.onChange(event)} /> {pArgs2ndToLast+
           ";"}
+        
           
     </div>
   }
@@ -28,27 +29,14 @@ class SingleCell extends Component {
 
   render(){
 
-    //ss:String = this.props.value ;
-    //ss= String.prototype.trim.call( this.props.value);
-    //var ss = this.props.value ;
     var ss =this.props.rowCSVData.trim() ;
-    //this.state.stateValue =ss ;
-    //var tt = ss.trim();
 
-    //ss.trim()
 
     if ( ss !="" ) // how value is passed from TableCSV to SingleCell
     {
       const parseArr = this.props.rowCSVData.split(',');
 
-      //var thisArg = parseArr[0]
       var thisArg = parseArr.shift();
-
-//      var args2ndToLast = (parseArr.pop()).join(',');
-      //var args2ndToLast = (parseArr.pop());
-      //var args2ndToLast = parseArr[1..];
-      //const joined2ndToLast = args2ndToLast.join(',');
-
 
       return this.renderSomething( thisArg , parseArr)  
     } else {
